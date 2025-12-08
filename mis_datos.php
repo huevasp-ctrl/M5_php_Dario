607239340b57e8bb3aee0d10306a4c0e49d9254e
huevasp-ctrl <huevasp@gmail.com>
2025-12-08 16:31:08 +0000

    Primeros pasos

---
+++ mis_datos.php
@@ -0,0 +1,9 @@
+<?php
+    $nombres = "Rubén Darío";
+    $apellidos = "Escamilla Gonzalez";
+    $correo = "rescamilla@gmail.com";
+    $cargo = "Jefe de Departamento";
+    $domicilio = "Iztapalapa, CDMX";
+
+    $nombre_completo = $nombres . " " . $apellidos;
+?>
